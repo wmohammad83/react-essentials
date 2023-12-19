496e31b65fa1a7e68cd41f093ea62c7848dbc2db
Waseem Mohammad <wmohammad83@icloud.com>
2023-12-19 22:35:37 +0000

    code cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -3,22 +3,33 @@ import Header from "./components/Header/Header";
 import CoreConcept from "./components/CoreConcept";
 import TabButton from "./components/TabButton";
 
-
 function App() {
   return (
     <div>
       <Header />
       <main>
         <section id="core-concepts">
-        <h2>Core Concepts</h2>
-        <ul>
-          {/* THIS IS THE SHORTER VERSION */}
-          <CoreConcept {...CORE_CONCEPTS[0]} />
-          {/* CAN ALSO USE THE BELOW */}
-          <CoreConcept title={CORE_CONCEPTS[1].title} description={CORE_CONCEPTS[1].description} image={CORE_CONCEPTS[1].image} />
-          <CoreConcept title={CORE_CONCEPTS[2].title} description={CORE_CONCEPTS[2].description} image={CORE_CONCEPTS[2].image} />
-          <CoreConcept title={CORE_CONCEPTS[3].title} description={CORE_CONCEPTS[3].description} image={CORE_CONCEPTS[3].image} />
-        </ul>
+          <h2>Core Concepts</h2>
+          <ul>
+            {/* THIS IS THE SHORTER VERSION */}
+            <CoreConcept {...CORE_CONCEPTS[0]} />
+            {/* CAN ALSO USE THE BELOW */}
+            <CoreConcept
+              title={CORE_CONCEPTS[1].title}
+              description={CORE_CONCEPTS[1].description}
+              image={CORE_CONCEPTS[1].image}
+            />
+            <CoreConcept
+              title={CORE_CONCEPTS[2].title}
+              description={CORE_CONCEPTS[2].description}
+              image={CORE_CONCEPTS[2].image}
+            />
+            <CoreConcept
+              title={CORE_CONCEPTS[3].title}
+              description={CORE_CONCEPTS[3].description}
+              image={CORE_CONCEPTS[3].image}
+            />
+          </ul>
         </section>
         <section id="examples">
           <h2>Examples</h2>
@@ -28,7 +39,6 @@ function App() {
             <TabButton>Props</TabButton>
             <TabButton>State</TabButton>
           </menu>
-
         </section>
       </main>
     </div>
