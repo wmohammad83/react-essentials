bc9b8b4e1f9b04085782c54a47a06966b0968249
Waseem Mohammad <wmohammad83@icloud.com>
2023-12-20 22:44:17 +0000

    data based on state from button click

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
-import { CORE_CONCEPTS } from "./data";
+import { useState } from "react";
+import { CORE_CONCEPTS, EXAMPLES } from "./data";
 import Header from "./components/Header/Header";
 import CoreConcept from "./components/CoreConcept";
 import TabButton from "./components/TabButton";
 
 function App() {
+  const [selectedTopic, setSelectedTopic] = useState("components");
+
   function handleSelect(selectedButton) {
     // selectedButton => "Component", "JSX", "Props", or "State"
-    alert(`You selected ${selectedButton}`);
+    setSelectedTopic(selectedButton);
+    // alert(`You selected ${selectedButton}`);
 }
 
   return (
@@ -44,6 +48,13 @@ function App() {
             <TabButton onSelect={() => handleSelect("props")}>Props</TabButton>
             <TabButton onSelect={() => handleSelect("state")}>State</TabButton>
           </menu>
+          <div id="tab-content">
+            <h3>{EXAMPLES[selectedTopic].title}</h3>
+            <p>{EXAMPLES[selectedTopic].description}</p>
+            <pre>
+              <code>{EXAMPLES[selectedTopic].code}</code>
+            </pre>
+          </div>
         </section>
       </main>
     </div>
