b55c8efef6f809b6adf4638de5340e66848d8b2f
Waseem Mohammad <wmohammad83@icloud.com>
2023-12-25 16:00:25 +0000

    splitting the component by feature and state

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,16 @@
-import { useState } from "react";
-import { CORE_CONCEPTS, EXAMPLES } from "./data";
 import Header from "./components/Header/Header";
-import CoreConcept from "./components/CoreConcept";
-import TabButton from "./components/TabButton";
+import CoreConcepts from "./components/CoreConcepts";
+import Examples from "./components/Examples";
 
 function App() {
-  const [selectedTopic, setSelectedTopic] = useState();
-
-  function handleSelect(selectedButton) {
-    // selectedButton => "Component", "JSX", "Props", or "State"
-    setSelectedTopic(selectedButton);
-    // alert(`You selected ${selectedButton}`);
-  }
-
   return (
-    <div>
+    <>
       <Header />
       <main>
-        <section id="core-concepts">
-          <h2>Core Concepts</h2>
-          <ul>
-            {CORE_CONCEPTS.map((conceptItem) => <CoreConcept key={conceptItem.title} {...conceptItem} />)}
-          </ul>
-        </section>
-        <section id="examples">
-          <h2>Examples</h2>
-          <menu>
-            <TabButton isSelected={selectedTopic === "components"} onSelect={() => handleSelect("components")}>
-              Component
-            </TabButton>
-            <TabButton isSelected={selectedTopic === "jsx"}  onSelect={() => handleSelect("jsx")}>JSX</TabButton>
-            <TabButton isSelected={selectedTopic === "props"}  onSelect={() => handleSelect("props")}>Props</TabButton>
-            <TabButton isSelected={selectedTopic === "state"}  onSelect={() => handleSelect("state")}>State</TabButton>
-          </menu>
-
-          {!selectedTopic ? (
-            <p>Please select a topic</p>
-          ) : (
-            <div id="tab-content">
-              <h3>{EXAMPLES[selectedTopic].title}</h3>
-              <p>{EXAMPLES[selectedTopic].description}</p>
-              <pre>
-                <code>{EXAMPLES[selectedTopic].code}</code>
-              </pre>
-            </div>
-          )}
-        </section>
+        <CoreConcepts />
+        <Examples />
       </main>
-    </div>
+    </>
   );
 }
 
--- src/components/CoreConcept.jsx
+++ src/components/CoreConcept.jsx
@@ -1,4 +1,4 @@
-export default function CoreConcept({ title, description, image }) {
+export default function CoreConcepts({ title, description, image }) {
     return (
       <li>
         <img src={image} alt={title} />
